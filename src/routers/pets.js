d4ac2d65701a716950075787dd37f2c3c346d509
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-04-01 16:19:51 +0100

    push

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -5,9 +5,29 @@ const db = require("../utils/database");
 const petsRouter = express.Router();
 
 petsRouter.get("/", (req, res)=> {
-    const selectAllPetsQuery = "SELECT * FROM pets";
+    let selectAllPetsQuery = "SELECT * FROM pets";
 
-    db.query(selectAllPetsQuery)
+    const selectValues = []
+    const queries = []
+
+    if(req.query.type) {
+        queries.push({col:'type', val: req.query.type})
+    }
+
+    if(req.query.microchip) {
+        queries.push({col: 'microchip', val: req.query.microchip})
+    }
+
+    if(queries.length>0) {
+        let whereClauses = []
+        queries.forEach((query, index) => {
+            whereClauses.push(`${query.col} = $${index+1}`)
+            selectValues.push(query.val)
+        })
+        selectAllPetsQuery += ' WHERE ' + whereClauses.join(' AND ')
+    }
+
+    db.query(selectAllPetsQuery, selectValues)
     .then((databaseResult) => {
         res.json({pets: databaseResult.rows});
     })
@@ -17,32 +37,10 @@ petsRouter.get("/", (req, res)=> {
     })
 })
 
-petsRouter.get("/?=type=", (req, res) => {
-    let type = req.query.id
-    const selectByTypeQuery = `SELECT * FROM pets WHERE type = $1`;
-    
-
-    db.query(selectByTypeQuery, type )
-    .then((databaseResult) => {
-        console.log(databaseResult.rows);
-        if (databaseResult.rowCount === 0) {
-          res.status(404);
-          res.json({ error: "Pet type does not EXIST" });
-        }
-        res.json({ book: databaseResult.rows[0] });
-      })
-      .catch((error) => {
-        res.status(500);
-        res.json({ error: "Unexpected Error" });
-        console.log(error);
-      });
-    
-})
-
 petsRouter.get("/:id", (req, res) => {
-    const selectBookByIdQuery = `SELECT * FROM pets WHERE id = $1`;
+    const selectPetByIdQuery = `SELECT * FROM pets WHERE id = $1`;
   
-    db.query(selectBookByIdQuery, [req.params.id])
+    db.query(selectPetByIdQuery, [req.params.id])
       .then((databaseResult) => {
         console.log(databaseResult.rows);
         if (databaseResult.rowCount === 0) {
@@ -87,6 +85,63 @@ petsRouter.get("/:id", (req, res) => {
       res.json({error: 'Unexpected Error'})
     })
   });
+
+petsRouter.delete("/:id", (req, res) => {
+    const deletePetsQuery = `DELETE FROM books WHERE id = $1 RETURNING *`
+
+    const deleteValues = [
+        req.params.id,
+    ]
+    db.query(deletePetsQuery, deleteValues)
+    .then((databaseResult) => {
+        if(databaseResult.rowCount === 0) {
+            res.status(404)
+            res.json({ error: "Pet does not exist"})
+        } else {
+            res.json({ pets: databaseResult.rows[0]})
+        }
+    })
+    .catch((error) => {
+        res.status(500)
+        res.json({ error: "unexpected error"})
+    })
+})
+
+petsRouter.put("/:id", (req, res) => {
+    const updatePetsQuery = `
+    UPDATE pets SET
+    name = $1,
+    age = $2,
+    type = $3,
+    breed = $4,
+    microchip = $5
+    WHERE id = $6
+    RETURNING *`
+
+const updateValues = [
+    req.body.name,
+    req.body.age,
+    req.body.type,
+    req.body.breed,
+    req.body.microchip,
+    req.body.id,
+]  
+
+db.query(updatePetsQuery, updateValues)
+.then(databaseResult => {
+    if(databaseResult.rowCount===0) {
+        res.status(404)
+        res.json({error: "pet does not exist"})
+    }
+    else {
+        res.json({ pets: databaseResult.rows[0]})
+    }
+})
+.catch(error => {
+    res.status(500)
+    res.json({error: "unexpected error"})
+})
+})
   
 
 module.exports = petsRouter
